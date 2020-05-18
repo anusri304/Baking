e9cfaf6989857f45b152bc1174d2fc3d03ac4abe
AnandhiD <Anandhi.Dharmarajan@ksdc.co.za>
2020-05-18 13:37:21 +0200

    Tablet/Phone determination fix

--- app/src/androidTest/java/com/example/baking/RecipeStepActivityRecyclerViewTest.java
+++ app/src/androidTest/java/com/example/baking/RecipeStepActivityRecyclerViewTest.java
@@ -1,19 +1,14 @@
 package com.example.baking;
 
-import android.app.Activity;
-import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
 import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
-import com.example.baking.activity.MainActivity;
 import com.example.baking.activity.RecipeStepActivity;
 import com.example.baking.utils.ApplicationConstants;
 import com.example.baking.utils.BakingUtils;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,11 +16,8 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
 public class RecipeStepActivityRecyclerViewTest {
